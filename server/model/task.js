075b23b0c8cb3b151ba67643cc6ac9f489ed691d
Ward <97world@gmail.com>
2018-03-31 01:57:07 +0800

    refactor: improve task model

--- server/model/task.js
+++ server/model/task.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const taskSchema = new Schema({
+  host: { type: String, require: true },
   url: { type: String, required: true, unique: true },
   checkedAt: { type: Date },
   latestURL: { type: String },
   latestEpisodeStr: { type: String },
   latestEpisodeNum: { type: Number },
   notifyStatus: { type: Number },               // 0: disable notify, 1: always notify, 2: notify when different episodeNum
-  disabled: { type: Boolean },
+  disabled: { type: Boolean, default: false },
   createAt: { type: Date, default: Date.now },
   updateAt: { type: Date, default: Date.now },
 });
